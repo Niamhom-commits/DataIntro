b82f9f0fdae598f1c638b0a1a36899f20a88e484
Niamh O'Mahony <niamhomahony966@gmail.com>
2021-05-06 16:09:35 +0100

    Start

--- Myproject.py
+++ Myproject.py
@@ -5,8 +5,11 @@ Created on Mon Apr 19 14:04:56 2021
 @author: omaho
 """
 
-print("Hello this is my project")
-
-print("New line")
-
-print("hello")
+import pandas as pd
+print("This is a new file")
+file='C:\\Users\\omaho\\downloads\public-register-03052021.xlsx'
+xls=pd.ExcelFile(file) 
+print(xls.sheet_names)
+df1=xls.parse('Public Register', skiprows=1)
+df2=xls.parse('Annual Reports', skiprows=1)
+print(df1.sort_values('CHY Number'))
